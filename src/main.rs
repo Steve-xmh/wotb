62e705f3e7d74e157f425a61d6ed587b469009ef
Steve-xmh <stevexmh@qq.com>
2021-07-06 13:54:59 +0800

    added lua feature

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use std::process::Command;
 use winapi::{
     shared::{minwindef::*, windef::*},
     um::winuser::*,
 };
-use std::process::Command;
+use serde::{Deserialize, Serialize};
+use mlua::prelude::*;
 
 #[derive(Debug)]
 struct EnumParam {
@@ -10,11 +12,20 @@ struct EnumParam {
     windows: Vec<HWND>,
 }
 
+#[derive(Debug, Deserialize)]
+struct InputData {
+    exec_file: String,
+    window_script: String,
+}
+
+#[derive(Debug, Serialize)]
+struct OutputData {
+
+}
+
 extern "system" fn enum_proc(hwnd: HWND, lparam: LPARAM) -> BOOL {
     let mut wpid = 0u32;
-    let enum_param = unsafe {
-        (lparam as *mut EnumParam).as_mut().unwrap()
-    };
+    let enum_param = unsafe { (lparam as *mut EnumParam).as_mut().unwrap() };
     unsafe {
         GetWindowThreadProcessId(hwnd, &mut wpid as *mut u32);
         if enum_param.pid == wpid {
@@ -44,24 +55,65 @@ fn list_windows_by_process(pid: u32) -> Vec<HWND> {
     }
 }
 
-fn test_window_open_time(exec_file: &str) {
-    let mut c = Command::new(exec_file);
+fn get_window_size(hwnd: HWND) -> (u32, u32) {
+    unsafe {
+        let mut rect = std::mem::zeroed();
+        GetWindowRect(hwnd, &mut rect);
+        ((rect.right - rect.left) as u32, (rect.bottom - rect.top) as u32)
+    }
+}
+
+fn test_window_open_time_once(input: &InputData, lua: &Lua, window_script: &LuaFunction) -> u128 {
+    let mut c = Command::new(&input.exec_file);
     let mut c = c.spawn().unwrap();
     let pid = c.id();
     let start_time = std::time::Instant::now();
     loop {
         std::thread::sleep(std::time::Duration::from_millis(0));
-        let wnds = (list_windows_by_process(pid));
-        let names = (wnds.iter().map(|&a| get_windows_name(a)).collect::<Vec<_>>());
-        if wnds.len() > 0 && names.contains(&"Plain Craft Launcher 2\u{3000}".into()) {
+        let wnds = list_windows_by_process(pid);
+        let wnds = wnds
+            .iter()
+            .map(|&a| {
+                let t = lua.create_table_with_capacity(0, 2).unwrap();
+                let name = get_windows_name(a);
+                let name = lua.create_string(&name).unwrap();
+                let size_t = lua.create_table_with_capacity(0, 2).unwrap();
+                let size = get_window_size(a);
+                size_t.set("width", size.0).unwrap();
+                size_t.set("height", size.1).unwrap();
+                t.set("name", name).unwrap();
+                t.set("size", size_t).unwrap();
+                t
+            });
+        let wnds_t = lua.create_sequence_from(wnds).unwrap();
+        lua.globals().set("windows", wnds_t).unwrap();
+        if window_script.call(()).unwrap_or(false) {
             break;
         }
     }
     c.kill().unwrap();
-    dbg!(start_time.elapsed().as_millis());
+    dbg!(start_time.elapsed().as_millis())
+}
+
+fn test_window_open_time(input: &InputData) {
+    let mut v = Vec::with_capacity(20);
+    let mut lua = Lua::new();
+    let window_script = lua.load(&input.window_script).into_function().unwrap();
+    for _ in 0..20 {
+        v.push(
+            test_window_open_time_once(&input, &lua, &window_script)
+        );
+    }
+    v.sort();
+    dbg!(v.iter().fold(0, |acc, &x| acc + x) / 20 as u128);
+    dbg!(v.first());
+    dbg!(v.last());
 }
 
 fn main() {
-    // test_window_open_time("D:/Documents/Programs/Minecraft/SharpCraftLauncher-20210704-17dbd0-i686.exe");
-    test_window_open_time("D:/Documents/Programs/Minecraft/Plain Craft Launcher 2.exe");
+    let mut args = std::env::args().collect::<Vec<_>>();
+    let output_path = args.pop().unwrap();
+    let input_path = args.pop().unwrap();
+    let input_data = serde_json::from_str(&std::fs::read_to_string(input_path).unwrap()).unwrap();
+    test_window_open_time(&input_data);
 }
